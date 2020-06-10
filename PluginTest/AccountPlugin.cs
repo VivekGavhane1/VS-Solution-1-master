a83a725588cf724e0c21aa80e114fe75f97de8aa
Vivek Gavhane <vivekgavhane1993@outlook.com>
2020-06-10 10:35:38 +0200

    //Now vivek from company laptop

--- PluginTest/AccountPlugin.cs
+++ PluginTest/AccountPlugin.cs
@@ -23,6 +23,8 @@ namespace PluginTest
             //from Laptop 1
             //Now again from my PC
 
+            //Now vivek from company laptop
+
             Entity entity = (Entity)execContext.InputParameters["Target"];
 
             if (entity.Contains("telephone1"))
